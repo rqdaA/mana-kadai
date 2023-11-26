eef2a410fafefe365c7c1364ac9b48803fd2eb35
rqda <info@rqda.wtf>
2023-11-27 01:34:17 +0900

    build(cron.txt-run.sh): remove use of run.sh and use docker-compose

--- run.dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-IMG_NAME="mana-kadai"
-PROG_LOC=$(pwd)
-if [ $(docker images $IMG_NAME | wc -l) -eq 1 ]; then
-	docker build -t $IMG_NAME .
-fi
-
-docker run --rm --env-file "$PROG_LOC/.env" "$IMG_NAME"
--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-IMG_NAME="mana-kadai"
-PROG_LOC="/opt/mana-kadai"
-if [ $(docker images $IMG_NAME | wc -l) -eq 1 ]; then
-	docker build -t $IMG_NAME .
-fi
-
-docker run --rm --env-file "$PROG_LOC/.env" "$IMG_NAME"
